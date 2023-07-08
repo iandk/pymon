1adba2364ea496cca3b148442472f23310830bf2
iandk <hi@ianklemm.de>
2023-07-08 18:57:23 +0000

    Fix missing package

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Update system packages
-apt-get update
+apt update
 
 # Set working directory
 mkdir -p /opt/pymon && cd /opt/pymon
 
 # Install Python 3 and pip
-apt-get install -y python3 python3-pip
+apt install -y python3 python3-pip python3.11-venv netcat-openbsd
 
 # Create a virtual environment
 python3 -m venv myvenv
@@ -24,5 +24,4 @@ deactivate
 # Setup systemd service file
 cp pymon.service /etc/systemd/system/pymon.service
 systemctl daemon-reload
-systemctl start pymon
 systemctl enable pymon
